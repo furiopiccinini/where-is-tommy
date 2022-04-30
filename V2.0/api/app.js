7c9109bd1cf2c713d0cde920cfb32fbaeb52b1fb
furiopiccinini <furiopiccinini@gmail.com>
2022-04-30 16:01:25 +0200

    FT-43 deleted useless route
    Deleted demo route

--- V2.0/api/app.js
+++ V2.0/api/app.js
@@ -6,11 +6,9 @@ var logger = require('morgan');
 var cors = require('cors'); // CORS module for Cross Origin Resource Sharing
 var bodyParser = require('body-parser');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api'); // require new route created
-var demoRouter = require('./routes/demo');
 
 var app = express();
 
@@ -30,7 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter); // new route to handle incoming JSON data
-app.use('/demo', demoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
